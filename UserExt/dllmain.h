cdd47cf37b6003fd7ca5240421d3920469d6f345
lan2405 <2405349439@qq.com>
2026-06-12 22:49:49 +0800

    添加项目文件。

---
+++ UserExt/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CUserExtModule : public ATL::CAtlDllModuleT< CUserExtModule >
+{
+public :
+	DECLARE_LIBID(LIBID_UserExtLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_USEREXT, "{cba63cca-446c-4ded-a68c-3be1e7fdec41}")
+};
+
+extern class CUserExtModule _AtlModule;
---
+++ UserExt/menuext.cpp
@@ -0,0 +1,27 @@
+﻿// menuext.cpp: Cmenuext 的实现
+
+#include "pch.h"
+#include "menuext.h"
+
+
+// Cmenuext
+
+HRESULT Cmenuext::Initialize(PCIDLIST_ABSOLUTE pidlFolder, IDataObject* pdtobj, HKEY hkeyProgID)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT Cmenuext::QueryContextMenu(HMENU hmenu, UINT indexMenu, UINT idCmdFirst, UINT idCmdLast, UINT uFlags)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT Cmenuext::InvokeCommand(LPCMINVOKECOMMANDINFO pici)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT Cmenuext::GetCommandString(UINT_PTR idcmd, UINT uflags, UINT* pwReserved, LPSTR pszName, UINT cchMax)
+{
+    return E_NOTIMPL;
+}
